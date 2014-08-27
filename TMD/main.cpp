08bb92ddd61bd091991e7922288deb48cdbc876e
Kacper Kołodziej <kacper@kolodziej.in>
2014-08-27 15:10:36 +0200

    TMD moved to separate repository

--- TMD/main.cpp
+++
@@ -1,5 +0,0 @@
-#include "wx/wx.h"
-#include "gui_client.hpp"
-#include "main_frame.hpp"
-
-IMPLEMENT_APP(gui_client);
--- TMD/src/debug_gui.cpp
+++
@@ -1,3 +0,0 @@
-#include "debug_gui.hpp"
-
-std::recursive_mutex debug::write_lock_;
