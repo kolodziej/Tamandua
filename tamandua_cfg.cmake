4fcbd0286361be41c8ec377ec8f0d92fe360c736
Kacper Kołodziej <kacper@kolodziej.in>
2014-09-02 18:39:33 +0200

    new version (version 0.2.0 omitted by mistake) 0.3.0-alpha

--- tamandua_cfg.cmake
+++ tamandua_cfg.cmake
@@ -1,6 +1,6 @@
 set(Tamandua_VERSION_MAJOR 0)
-set(Tamandua_VERSION_MINOR 1)
-set(Tamandua_VERSION_PATCH 1)
+set(Tamandua_VERSION_MINOR 3)
+set(Tamandua_VERSION_PATCH 0)
 set(Tamandua_RELEASE_TYPE alpha)
 
 set(LIB_NAME tamandua)
