3de91f624d2f5b60b2d102ef90378879f4632822
Kacper Kołodziej <kacper@kolodziej.in>
2014-09-02 13:13:06 +0200

    added necessary methods to headers of command_interpreter and module_base

--- headers/command_interpreter.hpp
+++ headers/command_interpreter.hpp
@@ -1,6 +1,9 @@
 #ifndef TAMANDUA_COMMAND_INTERPRETER_HPP
 #define TAMANDUA_COMMAND_INTERPRETER_HPP
 #include "types.hpp"
+#include "user.hpp"
+#include "message.hpp"
+#include <memory>
 #include <string>
 #include <map>
 #include <functional>
@@ -14,6 +17,7 @@ namespace tamandua
 
 		public:	
 			void register_command(std::string, std::functional<tamandua::processing_status(std::string)>);
+			tamandua::processing_status process(std::shared_ptr<user>, message &);
 	}
 }
 
--- headers/module_base.hpp
+++ headers/module_base.hpp
@@ -1,6 +1,10 @@
 #ifndef TAMANDUA_MODULE_BASE_HPP
 #define TAMANDUA_MODULE_BASE_HPP
 #include "types.hpp"
+#include "user.hpp"
+#include "message.hpp"
+#include "command_interpreter.hpp"
+#include <memory>
 #include <string>
 
 namespace tamandua
@@ -9,11 +13,14 @@ namespace tamandua
 	{
 		private:
 			std::string module_name_;
+			command_interpreter &interpreter_;
 
 		public:
-			module_base(std::string name) : module_name_(name) {}
+			module_base(std::string name, command_interpreter &interpreter) : module_name_(name), interpreter_(interpreter) {}
 
 			virtual bool is_hidden();
+			virtual void preprocessed(std::shared_ptr<user>, message &) = 0;
+			virtual void postprocessed(std::shared_ptr<user>, message &, processing_status) = 0;
 	};
 }
 
