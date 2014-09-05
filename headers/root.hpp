57eeb1deee83dd8e2528c182cb2564b3a6a90a98
Kacper Kołodziej <kacper@kolodziej.in>
2014-09-05 21:38:31 +0200

    developing root user

--- headers/root.hpp
+++ headers/root.hpp
@@ -1,7 +1,11 @@
 #ifndef TAMANDUA_ROOT_HPP
 #define TAMANDUA_ROOT_HPP
 #include "hidden_participant.hpp"
+#include "message.hpp"
 #include <string>
+#include <deque>
+#include <set>
+#include <map>
 
 namespace tamandua
 {
@@ -9,13 +13,23 @@ namespace tamandua
 		public hidden_participant
 	{
 		private:
+			static bool is_created_;
 			std::string password_;
+			std::set<id_number_t> auth_users_ids_;
+			std::deque<message> messages_;
+			std::map<std::string, (root::*)(const message&)> commands_;
 
 		public:
-			root(server &svr, std::string pass) : hidden_participant(svr, std::string("root")), password_(pass) {}
+			root(server, std::string);
 			
+			bool auth_user(id_number_t, std::string);
 			void read_message();
 			void deliver_message(const message&);
+
+		private:
+			void process_message_(message &);
+			void unauthorized_user_(id_number_t);
+
 	};
 }
 
--- src/root.cpp
+++ src/root.cpp
@@ -1,13 +1,51 @@
 #include "root.hpp"
+#include "utility.hpp"
 
 using namespace tamandua;
 
+root::root(server &svr, std::string pass) :
+	hidden_participant(svr, std::string("root")),
+	password_(pass)
+{
+	if (is_created_ == true)
+		throw int; // throw proper exception
+
+	is_created_ = true;
+}
+
+bool root::auth_user(id_number_t id, std::string password)
+{
+	if (password == password_)
+	{
+		auth_users_ids_.insert(id);
+		return true;
+	}
+
+	return false;
+}
+
 void root::read_message()
 {
-
+	message msg = messages_.front();
+	messages_.pop_front();
+	process_message_(msg);
 }
 
-void root::deliver_message(const message &message)
+void root::deliver_message(const message &msg)
 {
-
+	auto u = auth_users_ids_.find(msg.header.author);
+	if (u == auth_users_ids_.end())
+	{
+		unauthorized_user_(*u);
+	} else
+	{
+		messages_.push_back(msg);
+	}
 }
+
+void root::process_message_(const messsage &msg)
+{
+	auto params = split_params_std(msg.body);
+}
+
+bool root::is_created_ = false;
