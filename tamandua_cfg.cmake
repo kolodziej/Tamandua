7dc5df91ac9daef9d6a9ce28721894830b8c837e
Kacper Kołodziej <kacper@kolodziej.in>
2014-09-10 21:17:37 +0200

    version v0.5.1-alpha

--- tamandua_cfg.cmake
+++ tamandua_cfg.cmake
@@ -1,6 +1,6 @@
 set(Tamandua_VERSION_MAJOR 0)
 set(Tamandua_VERSION_MINOR 5)
-set(Tamandua_VERSION_PATCH 0)
+set(Tamandua_VERSION_PATCH 1)
 set(Tamandua_RELEASE_TYPE alpha)
 
 set(LIB_NAME tamandua)
