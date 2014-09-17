56f7fa786dc4bd2eb5017978653d2e82bef9d8d7
Kacper Kołodziej <kacper@kolodziej.in>
2014-09-17 18:50:41 +0200

    finished base implementation of register and auth commands

--- src/modules/registration_module.cpp
+++ src/modules/registration_module.cpp
@@ -17,10 +17,11 @@ void registration_module::cmd_register(std::shared_ptr<user> u, message &msg)
 	auto params = split_params_std(msg.body);
 	if (params.size() == 2)
 	{
-		std::string password = params[1];
+		std::string &password = params[1];
 		auto ins_p = users_.insert(std::make_pair(u->get_name(), password));
 		if (ins_p.second == true)
 		{
+			get_server().lock_username(u->get_name());
 			message_composer msgc(message_type::info_message, msg.header.group);
 			msgc << "Registration completed!";
 			u->deliver_message(msgc());
@@ -40,5 +41,28 @@ void registration_module::cmd_register(std::shared_ptr<user> u, message &msg)
 
 void registration_module::cmd_auth(std::shared_ptr<user> u, message &msg)
 {
-	
+	auto params = split_params_std(msg.body);
+	if (params.size() == 3)
+	{
+		std::string &username = params[1], &password = params[2];
+		auto nick = users_.find(username);
+		if (nick == users_.end())
+		{
+			message_composer msgc(error_message, msg.header.group);
+			msgc << "Username is not registered!";
+			u->deliver_message(msgc());
+		} else if (nick->second == password)
+		{
+			get_server().change_participant_name(u->get_name(), username);
+			u->set_name(username);
+			message_composer msgc(info_message, msg.header.group);
+			msgc << "You have been authorized as " << username << "!";
+			u->deliver_message(msgc());
+		} else
+		{
+			message_composer msgc(error_message, msg.header.group);
+			msgc << "Wrong password!";
+			u->deliver_message(msgc());
+		}
+	}
 }
