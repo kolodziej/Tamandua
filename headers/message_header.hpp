c0d95c31fe23cacf3ea67e13092410abb35bc685
Kacper Kołodziej <kacper@kolodziej.in>
2014-10-08 12:24:51 +0200

    modified header: added id and error_code fields

--- headers/message_header.hpp
+++ headers/message_header.hpp
@@ -7,11 +7,13 @@ namespace tamandua
 {
 	struct message_header
 	{
+		id_number_t id;
 		message_time_t utc_time;
 		id_number_t author;
 		id_number_t group;
 		message_type type;
 		message_size size;
+		status error_code;
 	};
 }
 
